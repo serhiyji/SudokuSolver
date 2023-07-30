a082250360ed11f9527c77c01e9274691f9f08f3
serhiyji <serhiy22823@gmail.com>
2023-07-30 18:21:35 +0300

    Add Singleton

---
+++ SudokuSolver.Extensions/Singleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Extensions
+{
+    public abstract class Singleton<T> where T : class, new()
+    {
+        private static T _instance;
+        public static T Instance => _instance ?? (_instance = new T());
+    }
+}
